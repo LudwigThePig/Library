26dadc2b19fcedba272831c77177e1ce36c79a84
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-10 12:47:45 +1300

    added rendering

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,14 @@
           response => response.json()
         )
         .then( (res)=>{
-          this.list = res.books.map((book) =>{
+          this.listOfBooks = res.books.map((book) =>{
             return { 
               title: book.bookTitle, 
               comments: book.comments,
               date: book.dateCreated
             }
           })
+        console.log(this.listOfBooks);
         })
         .catch( err => console.log(err) );
     },
@@ -47,6 +48,28 @@
     }
   };
   
+  const renderFunc = ()=>{
+    const list = fetchFunc.listOfBooks
+    console.log(list)
+    for (let i = 0; i < list.length; i++){
+      console.log(fetchFunc.listOfBooks[i])
+      const div = document.createElement('div');
+      const h3 = document.createElement('h2');
+      const p = document.createElement('p');
+      
+      div.setAttribute('class', 'book-container');
+      
+      h3.innerText = list[i].title;
+      p.innerText = `${list[i].comments.length} comments`;
+      
+      div.appendChild(h3);
+      div.appendChild(p);
+      
+      document.getElementById('display').appendChild(div);
+      
+    }
+  }
+  
   const form = {   
     addButton: document.getElementById('newBook'),
     addForm: document.getElementById('bookTitleToAdd'),
@@ -56,12 +79,12 @@
   const init = ()=>{
     
     fetchFunc.getBooks();
-    
+    setTimeout(function(){renderFunc()}, 500)
+        
     form.addButton.addEventListener('click', (e)=>{
       e.preventDefault();
       fetchFunc.addBook(form.addForm.value);
-      // window.location.reload();
-      form.addForm.value = '';
+      window.location.reload();
     })
     
     form.deleteAllButton.addEventListener('click', (e)=>{
