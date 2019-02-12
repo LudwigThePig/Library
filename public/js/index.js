1a193c990bb11502743d7c7935f9a552a5c9ea11
Morgan Galvin <morgangalvin@yahoo.com>
2019-02-13 08:19:49 +1300

    Created and styled comment elements for each book

--- public/js/index.js
+++ public/js/index.js
@@ -78,49 +78,58 @@ const fetchFunc = {
   }
 };
 
+
 const renderFunc = ()=>{
   const list = fetchFunc.listOfBooks
-  console.log(list)
+  
+  //initial render
   for (let i = 0; i < list.length; i++){
     const div = document.createElement('div');
     const h3 = document.createElement('h2');
     const p = document.createElement('p');
-    const ul = document.createElement('ul');
+    const addCom = document.createElement('span');
+    const seeCom = document.createElement('span');
+    const deleteBook = document.createElement('span');
+    
     const formEl = document.createElement('form');
     const inputField = document.createElement('input');
     const inputSubmit = document.createElement('input');
-
+    const hide = document.createElement('span');
 
     const comments = commentRender(list[i]);
     let bookId = list[i].id;
 
-    //getters
+    //attributes
     div.setAttribute('class', 'book-container');
+    let spanList = [addCom, seeCom, hide, deleteBook];
+    spanList.forEach(x => x.setAttribute('class', 'span-list'));
     formEl.setAttribute('id', bookId);
     inputField.setAttribute('type', 'text');
     inputField.setAttribute('name', 'commentForm');
     inputField.setAttribute('ref', 'commentForm');
     inputSubmit.setAttribute('type', 'submit');
     inputSubmit.setAttribute('name', 'commentbutton');
-
+    
     //event handling
-    formEl.setAttribute('onsubmit', 'commentHandler(this)')
-
+    formEl.setAttribute('onsubmit', 'commentHandler(this)');
 
 
     h3.innerText = list[i].title;
     p.innerText = `${list[i].comments.length} comments`;
+    addCom.innerText = 'add comment'; 
+    seeCom.innerText = 'view comments'; 
+    hide.innerText = 'hide'; 
+    deleteBook.innerText = 'delete book';
 
     formEl.appendChild(inputField);
     formEl.appendChild(inputSubmit);
 
     div.appendChild(h3);
     div.appendChild(p);
-    div.appendChild(ul);
-    // if (comments !== []){
-    //   div.appendChild(comments);
-    // }
     div.appendChild(formEl);
+    spanList.forEach(x => {
+      if (x != hide){ div.appendChild(x);
+      }});
 
     document.getElementById('display').appendChild(div);
 
@@ -146,9 +155,7 @@ function commentHandler(id){
 const form = {   
   addButton: document.getElementById('newBook'),
   addForm: document.getElementById('bookTitleToAdd'),
-  deleteAllButton: document.getElementById('deleteAllBooks'),
-  commentButton: document.getElementsByName('commentButton'),
-  commentForm: document.getElementsByName('commentForm')
+  deleteAllButton: document.getElementById('deleteAllBooks')
 };
 
 const init = ()=>{
